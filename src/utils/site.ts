9456ce67bbf5ab515ac9019e42ae3e9baa3b34e8
kuloteyesda <kuloteyesda@gmail.com>
2025-07-15 00:36:34 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Indo Meruchan';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Coli Sma Cindo Tante Tobrut Terbaru Viral';
-export const url = 'https://bocilsmatobrut.pages.dev';
-export const terbit = '2025-06-27T00:01:03Z';
